fe40c18bfbe6828f4ca4d3f79fb496b3ebcfdb97
Aguilar Alvarez Arturo <arturo_365ag@hotmail.com>
2016-06-27 11:23:48 -0500

    repositorio inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <heade>
+        <meta charset="utf-8">
+        <title>aplication</title>
+    </heade>
+    <body>
+        <h1>pagina principal</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores hic laboriosam repellendus tempore rem blanditiis, libero quo fugit, corporis, aliquam dolore doloribus. In corporis quas reprehenderit, consectetur, debitis blanditiis vitae.
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sequi expedita quasi totam facilis corporis vero laboriosam sapiente praesentium pariatur, mollitia laudantium aut sint placeat repellendus quaerat cupiditate ut facere?</p>
+        <h2>encabezado tipo h2</h2>
+    </body>
+</html>
